80dd4e09832d887f9b7c061fba269018862bf69e
unknown <hasankaradirek3@gmail.com>
2024-10-05 02:50:10 +0200

    query updated

--- src/main/java/com/sparkshare/demo/repository/EvStationRepository.java
+++ src/main/java/com/sparkshare/demo/repository/EvStationRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EvStationRepository extends JpaRepository<EvStation, Long> {
-    @Query(value = "SELECT * FROM stations s WHERE ST_DWithin(s.location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :radius)", nativeQuery = true)
-    Page<EvStation> findStationsWithinRadius(@Param("longitude") double longitude, @Param("latitude") double latitude, @Param("radius") double radius,Pageable pageable);
+    @Query(value = "SELECT *, ST_Distance(s.location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)) as distance FROM stations s WHERE ST_DWithin(s.location, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :radius) ORDER BY distance", nativeQuery = true)
+    Page<EvStation> findStationsWithinRadius(@Param("longitude") double longitude, @Param("latitude") double latitude, @Param("radius") double radius, Pageable pageable);
 
 }
